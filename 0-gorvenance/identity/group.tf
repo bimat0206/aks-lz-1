9899d5ce09b022137b4fff00cdbdf71a7c394e2c
manhht6 <manhht6@fpt.com>
2023-12-09 12:16:53 +0700

    fix external_senders_allowed

--- 0-gorvenance/identity/group.tf
+++ 0-gorvenance/identity/group.tf
@@ -3,11 +3,11 @@ resource "azuread_group" "groups" {
   for_each = var.enable_groups ? { for row in local.group_data : row.display_name => row } : {}
 
   display_name     = each.key
-  mail_enabled = true
+  #mail_enabled = true
   security_enabled = true
   description      = each.key
-  mail_nickname     = each.value.mail_nickname
-  types = ["Unified"]
+  #mail_nickname     = each.value.mail_nickname
+  #types = ["DynamicMembership"]
 }
 
 
