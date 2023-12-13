4be8bb043260cbce75ad9b49f7e7cac72917468c
manhht6 <manhht6@fpt.com>
2023-12-13 22:44:18 +0700

    add global region

--- 0-gorvenance/cost-management/main.tf
+++ 0-gorvenance/cost-management/main.tf
@@ -1,9 +1,11 @@
 resource "azurerm_cost_anomaly_alert" "example" {
   name            = "cost-anomaly-alert"
   display_name    = "cost-anomaly-alert"
-  email_subject   = "panasonic_cost_anomaly_alert "
+  email_subject   = "Halliburton_cost_anomaly_alert "
   email_addresses = ["manhht6@fpt.com"]
                 lifecycle {
     ignore_changes = [email_subject,name,display_name,email_addresses]
   }
 }
+
+#setting budgets and alerts at different scopes to proactively monitor the cost.
